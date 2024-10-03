bb5d692a30c85007b7a7acc788855d5626324fa9
Hivan <hivannolli11@gmail.com>
2024-10-02 21:08:33 -0300

    first commit

--- server.js
+++ server.js
@@ -3,14 +3,19 @@ const http = require('http');
 const requestListener = (req, res) => {
     if (req.url === '/') {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Hello Giovane, desculpe estar faltando as aulas');
-       } else {
+        res.end('hello');
+       } else if (req.url === '/sobre') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Sou um dev preguiçoso');
+        } else if (req.url === '/contato') {
+         res.writeHead(200, { 'Content-Type': 'text/plain' });
+         res.end('Contato: Email - Engs-hivanrossinolli@camporeal.edu.br');
+        } else {    
         res.wrideHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Não encontrei nada'); 
+        res.end('nada aqui'); 
     }
 
 };
-
 
 const server = http.createServer(requestListener);
 
